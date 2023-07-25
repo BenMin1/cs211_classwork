9a64a69f399262b2ea962f51e91ae3a536e560e7
BenMin1 <124104952+BenMin1@users.noreply.github.com>
2023-07-25 19:00:03 -0400

    Create FizzBuzz.cpp

---
+++ assignments/FizzBuzz.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout << "Enter largest integer ";
+  cin >> n;
+  
+  for(int i = 1; i <= n; i++){
+    if(i % 3 != 0 && i % 5 != 0) cout << i;
+    if(i % 3 == 0) cout << "Fizz";
+    if(i % 5 == 0) cout << "Buzz";
+    cout << endl;
+  }
+  return 0;
+}
